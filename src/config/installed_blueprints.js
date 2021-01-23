d0e15fe6ef6a174a5a121b62adb9b490303a3a77
Crowdbotics <team@crowdbotics.com>
2021-01-23 09:29:47 +0000

    add package blueprint-react-native-contacts-screen

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -1,5 +1,6 @@
 export const installed_blueprints = [
   //@BlueprintInsertion
+{ name: 'Contacts195595', human_name: 'Contacts', access_route: 'Contacts195595'},
 
   // you can add more installed blueprints here
   // access route is the route nate given to navigator
--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -5,6 +5,7 @@ import {createDrawerNavigator} from 'react-navigation-drawer';
 import SplashScreen from "../features/SplashScreen";
 import SideMenu from './sideMenu';
 //@BlueprintImportInsertion
+import Contacts195595Navigator from '../features/Contacts195595/navigator';
 
 /**
  * new navigators can be imported here
@@ -13,6 +14,7 @@ import SideMenu from './sideMenu';
 const AppNavigator = {
 
     //@BlueprintNavigationInsertion
+Contacts195595: { screen: Contacts195595Navigator },
 
     /** new navigators can be added here */
     SplashScreen: {
